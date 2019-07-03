93b29b4d018360702032bda0703aaf426a70e389
tinavanschelt <tinavanschelt@gmail.com>
2019-07-03 13:16:51 +0100

    update server

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'build')));
+const PORT = process.env.PORT || 8080;
+const DIST_DIR = path.join(__dirname, '../build');
+const HTML_FILE = path.join(DIST_DIR, 'index.html');
+
+app.use(express.static(path.join(__dirname, DIST_DIR)));
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/build/index.html');
+  response.sendFile(__dirname + HTML_FILE);
 });
 
-app.listen(PORT, error => (
+app.listen(PORT, error =>
   error
     ? console.error(error)
-    : console.info(`Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`)
-));
+    : console.info(
+        `Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`
+      )
+);
